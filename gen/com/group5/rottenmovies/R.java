607cead4d2b19019772a469fc6d1bcb2f2f0a273
Tejas Padekar <padekar@yahoo-inc.com>
2013-06-19 23:32:22 -0700

    Minor UI changes to Carousel

--- gen/com/group5/rottenmovies/R.java
+++ gen/com/group5/rottenmovies/R.java
@@ -25,16 +25,17 @@ public final class R {
         public static final int poster_default=0x7f020001;
     }
     public static final class id {
-        public static final int action_settings=0x7f080009;
-        public static final int carousel=0x7f080005;
-        public static final int carousel_container=0x7f080004;
+        public static final int action_settings=0x7f08000a;
+        public static final int carousel=0x7f080006;
+        public static final int carousel_container=0x7f080005;
         public static final int carousel_title=0x7f080003;
         public static final int home=0x7f080000;
         public static final int home_layout=0x7f080001;
-        public static final int moviePoster=0x7f080006;
-        public static final int movieRating=0x7f080007;
-        public static final int movieTitle=0x7f080008;
+        public static final int moviePoster=0x7f080007;
+        public static final int movieRating=0x7f080008;
+        public static final int movieTitle=0x7f080009;
         public static final int movie_carousel=0x7f080002;
+        public static final int separator=0x7f080004;
     }
     public static final class layout {
         public static final int activity_home=0x7f030000;
