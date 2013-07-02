794cb8da035e119cf565f5627711e758050a18e5
Tejas Padekar <padekar@yahoo-inc.com>
2013-07-02 00:01:27 -0700

    testing merge

--- src/com/group5/rottenmovies/uielements/MovieCardAdapter.java
+++ src/com/group5/rottenmovies/uielements/MovieCardAdapter.java
@@ -11,8 +11,6 @@ import android.content.Intent;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.view.animation.Animation;
-import android.view.animation.AnimationUtils;
 import android.widget.ArrayAdapter;
 import android.widget.ImageView;
 import android.widget.RatingBar;
